09d36d70f0297343cf35d5d93e72e9fc7b81592c
Guilherme José <101995065+GuilhermeJQA@users.noreply.github.com>
2022-08-30 12:55:50 -0300

    Add files via upload

---
+++ TedJavaScript/ProjetoJavaScript.js
@@ -0,0 +1,26 @@
+var i, altura, sexo, maioralt = 0, menoralt = 0, mulh = 0, hom = 0, mediahom = 0, contamulh = 0, contahom = 0;
+
+            for(i = 1 ;i <= 15; i++) {        
+                altura = parseInt(prompt("Informe sua altura["+i+"]:"));
+                sexo = prompt("Sexo: M ou F:");
+
+
+                if(i == 1) {
+                    menoralt = altura;
+                } if(altura > maioralt) { 
+                    maioralt = altura;
+                } if(altura < menoralt) { 
+                    menoralt = altura;
+                } if(sexo == 'F') {
+                    mulh += altura;
+                    contamulh++;
+                } else if(sexo == 'M') {
+                    hom += altura;
+                    contahom++;
+                }
+            }
+            mediahom = hom/contahom;
+            alert("A maior altura é: "+maioralt); 
+            alert("A menor altura é: "+menoralt);
+            alert("A média dos homens é: "+mediahom);
+            alert("O número de mulheres é: "+contamulh);
